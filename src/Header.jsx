2d37ae84eff6c0b19dfed76a50ff77615d98a851
Carlos Pires <extraterra1@gmail.com>
2023-10-31 10:31:33 +0000

    feat: add icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,20 @@
+import { Icon } from '@iconify/react';
 import './css/Header.css';
 
 export default function Header() {
   return (
     <nav className="header">
-      <span className="title">CV Generator</span>
+      <span className="title">Memory Game</span>
       <div className="socials">
-        <span>🌧️</span>
-        <span>🌧️</span>
-        <span>🌧️</span>
+        <span>
+          <Icon icon="ion:logo-github" />
+        </span>
+        <span>
+          <Icon icon="ion:logo-twitter" />
+        </span>
+        <span>
+          <Icon icon="ion:logo-youtube" />
+        </span>
       </div>
     </nav>
   );
